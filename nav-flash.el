7312731bc8a41813c618aefb2f64993f35888143
Roland Walker <walker@pobox.com>
2015-02-23 09:12:56 -0500

    doc/comment: copyright date/format

--- nav-flash.el
+++ nav-flash.el
@@ -1,6 +1,6 @@
 ;;; nav-flash.el --- Briefly highlight the current line
 ;;
-;; Copyright (c) 2012-13 Roland Walker
+;; Copyright (c) 2012-2015 Roland Walker
 ;;
 ;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/nav-flash
